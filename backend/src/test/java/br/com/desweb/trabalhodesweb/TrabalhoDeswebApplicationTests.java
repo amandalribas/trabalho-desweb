0361f0b83edae231ea982ad2b33b000e016e71fe
Amanda Lemos Ribas <amandaribas@id.uff.br>
2026-06-20 15:52:13 -0300

    base projeto
    precisa dar npm i na pasta frontend

---
+++ backend/src/test/java/br/com/desweb/trabalhodesweb/TrabalhoDeswebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desweb.trabalhodesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoDeswebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
