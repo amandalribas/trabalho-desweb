08c61445aa188c218866beebb90250bee76b2b20
Guilherme Fontoura <guilhermedhfontoura@gmail.com>
2026-06-26 19:56:34 -0300

    corrige erro do create

--- backend/src/main/java/br/com/desweb/trabalhodesweb/dto/EventoCreate.java
+++ backend/src/main/java/br/com/desweb/trabalhodesweb/dto/EventoCreate.java
@@ -1,13 +1,11 @@
 package br.com.desweb.trabalhodesweb.dto;
 
 import br.com.desweb.trabalhodesweb.model.TipoEvento;
-import lombok.Data;
 
-@Data
-public class EventoCreate {
-    private TipoEvento tipoEvento;
-    private Long timeId;
-    private Long jogoId;
-    private String jogador;
-}
+public record EventoCreate (
+    TipoEvento tipoEvento,
+    Long timeId,
+    Long jogoId,
+    String jogador
+) {}
 
